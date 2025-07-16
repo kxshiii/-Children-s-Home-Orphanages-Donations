ccea19b74714dca9f2e767927233edffc28f74eb
kxshiii <brian.gikaru@student.moringaschool.com>
2025-07-16 13:31:53 +0300

    working on components

---
+++ front-end/jest.config.js
@@ -0,0 +1,7 @@
+export default {
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  },
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"]
+};
