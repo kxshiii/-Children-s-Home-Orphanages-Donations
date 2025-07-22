20a41dce905ece7f496230769cba2511bad87780
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-07-22 11:06:47 +0300

    button

--- front-end/src/components/Button.jsx
+++ front-end/src/components/Button.jsx
@@ -1,1 +1,15 @@
- 
+ import React from 'react';
+
+const Button = ({ children, onClick, type = 'button', className = '' }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      className={`bg-blue-600 text-white px-4 py-2 rounded-2xl hover:bg-blue-700 transition-all duration-200 ${className}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
