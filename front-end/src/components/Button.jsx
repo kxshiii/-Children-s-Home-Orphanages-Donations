1b65804236982d99f844e2469a751e8a9c12335a
kxshiii <brian.gikaru@student.moringaschool.com>
2025-07-22 12:42:02 +0300

    fixing collective errors

--- front-end/src/components/Button.jsx
+++ front-end/src/components/Button.jsx
@@ -1,26 +1,14 @@
-<<<<<<< HEAD
- import React from 'react';
-
-const Button = ({ children, onClick, type = 'button', className = '' }) => {
-=======
 import React from "react";
 
-const Button = ({ children, text, onClick, type = "button", ...props }) => {
->>>>>>> d6f341b9e0e34970ef97fa1fb73f878ccf34f3c0
+const Button = ({ children, text, onClick, type = "button", className = "", ...props }) => {
   return (
     <button
       type={type}
       onClick={onClick}
-<<<<<<< HEAD
-      className={`bg-blue-600 text-white px-4 py-2 rounded-2xl hover:bg-blue-700 transition-all duration-200 ${className}`}
-    >
-      {children}
-=======
-      className="bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded"
+      className={`bg-blue-500 hover:bg-blue-600 text-white font-medium py-2 px-4 rounded ${className}`}
       {...props}
     >
       {children || text}
->>>>>>> d6f341b9e0e34970ef97fa1fb73f878ccf34f3c0
     </button>
   );
 };
